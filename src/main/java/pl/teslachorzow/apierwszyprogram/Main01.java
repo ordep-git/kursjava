0b134fb0492e6fff34ca6038d344f3f2b7c49adb
unknown <ordep@wp.pl>
2021-10-28 16:52:26 +0200

    Moj pierwszy commit2

---
+++ src/main/java/pl/teslachorzow/apierwszyprogram/Main01.java
@@ -0,0 +1,10 @@
+package pl.teslachorzow.apierwszyprogram;
+
+//      psvm
+
+public class Main01 {
+    public static void main(String[] args) {
+//       sout
+        System.out.println("Mój pierwszy program w Java");
+    }
+}
